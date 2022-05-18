de68361f5829adf9c3c4052bfb599a560402ee52
Kyle Stevens <kstevens715@gmail.com>
2022-05-18 06:28:44 -0400

    turbo frame transactions index

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,31 @@
 class TransactionsController < ApplicationController
   def index
-    @transactions = Transaction.all
-    pp @transactions
+    @new_transaction = Transaction.new
+  end
+
+  def create
+    transaction = Transaction.new(transaction_params)
+    transaction.account_id = 1
+    transaction.save!
+
+    if transaction.save
+      render_transactions_table
+    else
+      raise 'implement'
+    end
+  end
+
+  def table
+    render_transactions_table
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:amount_cents, :category_id, :note, :payee_id, :transaction_date)
+  end
+
+  def render_transactions_table
+    render turbo_stream: turbo_stream.replace('transactions_table', partial: "transactions/table", locals: { transactions: Transaction.all })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'transactions#index'
-  resources :transactions, only: [:index]
+  resources :transactions, only: [:create, :index] do
+    collection do
+      get :table
+    end
+  end
 end
